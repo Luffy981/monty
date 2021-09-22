c187961ab668b8d642d774b8bc8b4525626ff0c4
nitaly31 <geraldine.nitaly@gmail.com>
2021-09-22 16:59:38 -0700

    Elbow mod betty style

--- main.c
+++ main.c
@@ -1,4 +1,9 @@
 #include "monty.h"
+
+/**
+ * free_dlistint - function to add elements to list
+ * @head: Pointer of type list
+ */
 void free_dlistint(stack_t *head)
 {
 	stack_t *tmp = head;
@@ -10,7 +15,15 @@ void free_dlistint(stack_t *head)
 	free_dlistint(head->next);
 	free(tmp);
 }
-int main( __attribute__((unused)) int argc, char **argv)
+
+/**
+ * main - Main function
+ *
+ * @argc: Number of arguments
+ * @argv: Array of arguments of main.
+ * Return: 1 if succes
+ */
+int main(__attribute__((unused)) int argc, char **argv)
 {
 	vars_t vars = {NULL};
 	char *delim = "\n";
@@ -18,42 +31,46 @@ int main( __attribute__((unused)) int argc, char **argv)
 	int a;
 
 	stack_t *head = NULL;
-    if (argc != 2)
-    {
-        fprintf(stderr, "USAGE: monty file\n");
-        exit(EXIT_FAILURE);
-    }
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
 	vars.buffer = file_read(argv[1]);
 	/* push 3 \n push 4  \n pall \n pop \n */
 	vars.arrays = tokenizer(vars.buffer, delim);
 	/*
-	  push 3
-	  push 4
-	  pall
-	  pop */
+	 * push 3
+	 * push 4
+	 * pall
+	 * pop
+	 */
 	if (vars.arrays == NULL)
 	{
 		printf("Data invalid");
-	} else
+	}
+	else
 	{
-		for(a = 0 ; vars.arrays[a]  != NULL ; a++)
+		for (a = 0 ; vars.arrays[a]  != NULL ; a++)
 		{
-            vars.line_number = a + 1;
+			vars.line_number = a + 1;
 			vars.tokens = tokenizer(vars.arrays[a], " ");
 			f = get_op_fuctions(&vars, &head);
 			if (f == NULL)
 			{
-                fprintf(stderr, "L%d: unknown instruction %s\n", vars.line_number, vars.arrays[a]);
-                exit(EXIT_FAILURE);
-			} else
+				fprintf(stderr, "L%d: unknown instruction %s\n", vars.line_number, vars.arrays[a]);
+				exit(EXIT_FAILURE);
+			}
+			else
 			{
 				f(&vars, &head);
 			}
-            free(vars.tokens);
+			free(vars.tokens);
 		}
 	}
-    free(vars.arrays);
-    free(vars.buffer);
-    free_dlistint(head);
-	return(1);
+	free(vars.arrays);
+	free(vars.buffer);
+	free_dlistint(head);
+	return (1);
 }

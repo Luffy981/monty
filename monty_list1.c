6fd26dd2fad284de1db01748923b35d84c31ea5a
nitaly31 <geraldine.nitaly@gmail.com>
2021-09-23 09:40:48 -0700

    Division of functions by files

---
+++ monty_list1.c
@@ -0,0 +1,122 @@
+#include "monty.h"
+
+/**
+ * add_monty - adds the top two elements of the stack
+ * @vars: stack quantity
+ * @head: top stack
+ * Return: returns 0 if correct
+ */
+char add_monty(vars_t *vars, stack_t **head)
+{
+	int sum = 0;
+	stack_t *node = *head;
+	stack_t *tmp = *head;
+
+	if (node->next == NULL || node == NULL)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", vars->line_number);
+		exit(EXIT_FAILURE);
+	}
+	sum = node->n + node->next->n;
+	node->next->n = sum;
+	*head = node->next;
+	node->next->prev = NULL;
+	free(tmp);
+	return (0);
+}
+
+/**
+ * nop_monty - The opcode nop doesn’t do anything
+ * @vars: stack quantity
+ * @head: top stack
+ * Return:  returns 0 if correct
+ */
+char nop_monty(vars_t *vars, stack_t **head)
+{
+	UNUSED(vars);
+	UNUSED(head);
+	return (0);
+}
+
+/**
+ * sub_monty - subtracts the top element of the stack from the
+ * second top element of the stack
+ * @vars: stack quantity
+ * @head: top stack
+ * Return: returns 0 if correct
+ */
+char sub_monty(vars_t *vars, stack_t **head)
+{
+	int sub = 0;
+	stack_t *node = *head;
+	stack_t *tmp = *head;
+
+	if (node->next == NULL || node == NULL)
+	{
+		fprintf(stderr, "L%d: can't sub, stack too short\n", vars->line_number);
+		exit(EXIT_FAILURE);
+	}
+	sub = abs(node->n - node->next->n);
+	node->next->n = sub;
+	*head = node->next;
+	node->next->prev = NULL;
+	free(tmp);
+	return (0);
+}
+
+/**
+ * div_monty - divides the second top element of the stack by the top
+ * element of the stack
+ * @vars: stack quantity
+ * @head: top stack
+ * Return: returns 0 if correct
+ */
+char div_monty(vars_t *vars, stack_t **head)
+{
+	int div = 0;
+	stack_t *node = *head;
+	stack_t *tmp = *head;
+
+	if (node->next == NULL || node == NULL)
+	{
+		fprintf(stderr, "L%d: can't div, stack too short\n", vars->line_number);
+		exit(EXIT_FAILURE);
+	}
+	if (node->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", vars->line_number);
+		exit(EXIT_FAILURE);
+	}
+	div = node->n / node->next->n;
+	node->next->n = div;
+	*head = node->next;
+	node->next->prev = NULL;
+	free(tmp);
+	return (0);
+}
+
+/**
+ * mul_monty - multiplies the second top element of the stack with the
+ * top element of the stack
+ * @vars: stack quantity
+ * @head: top stack
+ * Return: returns 0 if correct
+ */
+char mul_monty(vars_t *vars, stack_t **head)
+{
+	int mul = 0;
+	stack_t *node = *head;
+	stack_t *tmp = *head;
+
+	if (node->next == NULL || node == NULL)
+	{
+		fprintf(stderr, "L%d: can't mul, stack too short\n", vars->line_number);
+		exit(EXIT_FAILURE);
+	}
+	mul = node->n * node->next->n;
+	node->next->n = mul;
+	*head = node->next;
+	node->next->prev = NULL;
+	free(tmp);
+	return (0);
+}
---
+++ monty_list2.c
@@ -0,0 +1,96 @@
+#include "monty.h"
+
+/**
+ * mod_monty - computes the rest of the division of the second top element of
+ * the stack by the top element of the stack
+ * @vars: stack quantity
+ * @head: top stack
+ * Return: returns 0 if correct
+ */
+char mod_monty(vars_t *vars, stack_t **head)
+{
+	int mod = 0;
+	stack_t *node = *head;
+	stack_t *tmp = *head;
+
+	if (node->next == NULL || node == NULL)
+	{
+		fprintf(stderr, "L%d: can't mod, stack too short\n", vars->line_number);
+		exit(EXIT_FAILURE);
+	}
+	if (node->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", vars->line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	mod = node->n % node->next->n;
+	node->next->n = mod;
+	*head = node->next;
+	node->next->prev = NULL;
+	free(tmp);
+	return (0);
+}
+/**
+ * comment_monty - Special character that represents a comment
+ * @vars: stack quantity
+ * @head: top stack
+ * Return: returns 0 if correct
+ */
+char comment_monty(vars_t *vars, stack_t **head)
+{
+	UNUSED(vars);
+	UNUSED(head);
+	return (0);
+}
+
+/**
+ * pchar_monty - prints the char at the top of the stack, followed by a new
+ * line
+ * @vars: stack quantity
+ * @head: top stack
+ * Return: returns 0 if correct
+ */
+char pchar_monty(vars_t *vars, stack_t **head)
+{
+	stack_t *node = *head;
+
+	UNUSED(vars);
+	if (node->n < 0 || node->n > 255)
+	{
+		fprintf(stderr, "L%d: can't pchar, value out of range", vars->line_number);
+		exit(EXIT_FAILURE);
+	}
+	if (node == NULL)
+	{
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", vars->line_number);
+		exit(EXIT_FAILURE);
+	}
+	putchar(node->n);
+	putchar('\n');
+	return (0);
+}
+
+/**
+ * swap_monty - swaps the top two elements of the stack
+ * @vars: stack quantity
+ * @head: top stack
+ * Return: returns 0 if correct
+ */
+char swap_monty(vars_t *vars, stack_t **head)
+{
+	int tmp;
+	int count;
+
+	count = len_stack(&*head);
+
+	if (count < 2)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", vars->line_number);
+		exit(EXIT_FAILURE);
+	}
+	tmp = (*head)->n;
+	(*head)->n = (*head)->next->n;
+	(*head)->next->n = tmp;
+	return (0);
+}

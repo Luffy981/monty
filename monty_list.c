5d699414e255ace8347bcbe299a2e1905a3d2721
luffy981 <jhonsmith981@hotmail.com>
2021-09-22 22:10:46 -0500

    test 7

--- monty_list.c
+++ monty_list.c
@@ -136,3 +136,192 @@ char nop_monty(vars_t *vars, stack_t ** head)
     UNUSED(head);
     return(0);
 }
+
+char sub_monty(vars_t *vars, stack_t **head)
+{
+    int sub = 0;
+    stack_t *node = *head;
+    stack_t *tmp = *head;
+
+    if (node->next == NULL || node == NULL)
+    {
+        fprintf(stderr, "L%d: can't sub, stack too short\n", vars->line_number);
+        exit(EXIT_FAILURE);
+    }
+    sub = abs(node->n - node->next->n);
+    node->next->n = sub;
+    *head = node->next;
+    node->next->prev = NULL;
+    free(tmp);
+    return (0);
+}
+
+char div_monty(vars_t *vars, stack_t **head)
+{
+    int div = 0;
+    stack_t *node = *head;
+    stack_t *tmp = *head;
+
+    if (node->next == NULL || node == NULL)
+    {
+        fprintf(stderr, "L%d: can't div, stack too short\n", vars->line_number);
+        exit(EXIT_FAILURE);
+    }
+    if(node->n == 0)
+    {
+        fprintf(stderr, "L%d: division by zero\n", vars->line_number);
+        exit(EXIT_FAILURE);
+    }
+    div = node->n / node->next->n;
+    node->next->n = div;
+    *head = node->next;
+    node->next->prev = NULL;
+    free(tmp);
+    return (0);
+}
+
+char mul_monty(vars_t *vars, stack_t **head)
+{
+    int mul = 0;
+    stack_t *node = *head;
+    stack_t *tmp = *head;
+
+    if (node->next == NULL || node == NULL)
+    {
+        fprintf(stderr, "L%d: can't mul, stack too short\n", vars->line_number);
+        exit(EXIT_FAILURE);
+    }
+    mul = node->n * node->next->n;
+    node->next->n = mul;
+    *head = node->next;
+    node->next->prev = NULL;
+    free(tmp);
+    return (0);
+}
+
+char mod_monty(vars_t *vars, stack_t **head)
+{
+    int mod = 0;
+    stack_t *node = *head;
+    stack_t *tmp = *head;
+
+    if (node->next == NULL || node == NULL)
+    {
+        fprintf(stderr, "L%d: can't mod, stack too short\n", vars->line_number);
+        exit(EXIT_FAILURE);
+    }
+    if(node->n == 0)
+    {
+        fprintf(stderr, "L%d: division by zero\n", vars->line_number);
+        exit(EXIT_FAILURE);
+    }
+
+    mod = node->n % node->next->n;
+    node->next->n = mod;
+    *head = node->next;
+    node->next->prev = NULL;
+    free(tmp);
+    return (0);
+
+}
+
+char comment_monty(vars_t *vars, stack_t ** head)
+{
+    UNUSED(vars);
+    UNUSED(head);
+    return(0);
+}
+
+char pchar_monty(vars_t *vars, stack_t **head)
+{
+    stack_t *node = *head;
+    
+    UNUSED(vars);
+    if (node->n < 0 || node->n >255)
+    {
+        fprintf(stderr, "L%d: can't pchar, value out of range", vars->line_number);
+        exit(EXIT_FAILURE);
+    }
+	if (node == NULL)
+    {
+        fprintf(stderr, "L%d: can't pchar, stack empty\n", vars->line_number);
+        exit(EXIT_FAILURE);
+    }
+    putchar(node->n);
+    putchar('\n');
+    return (0);
+}
+
+char swap_monty(vars_t *vars, stack_t **head)
+{
+    int tmp;
+    int count;
+    count = len_stack(&*head);
+
+    if(count < 2)
+    {
+        fprintf(stderr, "L%d: can't swap, stack too short\n", vars->line_number);
+        exit(EXIT_FAILURE);
+    }
+    tmp = (*head)->n;
+    (*head)->n = (*head)->next->n;
+    (*head)->next->n = tmp;
+    return(0);
+}
+
+int len_stack(stack_t **head)
+{
+    int count = 0;
+    stack_t *stack = *head;
+
+	while (stack != NULL)
+	{
+		stack = stack->next;
+		count++;
+	}
+	return (count);
+}
+
+char pstr_monty(vars_t *vars, stack_t **head)
+{
+    char *buffer = NULL;
+    size_t m = 1;
+    size_t i = 0;
+    stack_t *stack = *head;
+    UNUSED(vars);
+    if(stack == NULL)
+    {
+        putchar('\n');
+        return(1);
+    }
+    buffer = malloc(sizeof(char) * m);
+    if(buffer == NULL)
+    {
+        fprintf(stderr, "Error: malloc failed\n");
+        exit(EXIT_FAILURE);
+    }
+    while(stack != NULL)
+    {
+        if(stack->n == 0 || stack->n < 0 || stack->n > 255)
+        {
+            i++;
+            break;
+        }
+        buffer[i] = stack->n;
+        stack = stack->next;
+        i++;
+        if(i >= m)
+        {
+            buffer = _realloc(buffer,sizeof(char) * i, sizeof(char) * (i + 1));
+            if(buffer == NULL)
+            {
+                fprintf(stderr, "Error: realloc failed\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+    buffer[i] = '\0';
+    printf("%s\n", buffer);
+    return(0);
+}
+

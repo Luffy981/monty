ad192034c84d2bbbb6d1474739456aea5993dacf
nitaly31 <geraldine.nitaly@gmail.com>
2021-09-22 17:18:52 -0700

    Betty style code modification

--- read_file.c
+++ read_file.c
@@ -1,5 +1,10 @@
 #include "monty.h"
 
+/**
+ * file_read - Function that reads a file
+ * @filename: filename to read
+ * Return: Buffer
+ */
 char *file_read(char *filename)
 {
 	ssize_t size;
@@ -8,28 +13,28 @@ char *file_read(char *filename)
 	struct stat st;
 
 	if (filename == NULL)
-		return(NULL);
+		return (NULL);
 	fd = open(filename, O_CREAT | O_RDONLY, 600);
 	if (fd == -1)
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", filename);
-        exit(EXIT_FAILURE);
-    }
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", filename);
+		exit(EXIT_FAILURE);
+	}
 	fstat(fd, &st);
 	buffer = malloc(sizeof(char) * st.st_size + 1);
 	if (buffer == NULL)
 	{
-        fprintf(stderr, "Error: malloc failed\n");
+		fprintf(stderr, "Error: malloc failed\n");
 		close(fd);
-		return(NULL);
+		return (NULL);
 	}
 	size = read(fd, buffer, st.st_size);
 	if (size == -1)
 	{
-        fprintf(stderr, "Error: Can't read file %s\n", filename);
+		fprintf(stderr, "Error: Can't read file %s\n", filename);
 		close(fd);
 		free(buffer);
-		return(NULL);
+		return (NULL);
 	}
 	close(fd);
 	buffer[size] = '\0';
